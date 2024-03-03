d9851c2c9c5624ac85d3f6b3fae6ff8b029f6d04
Rahul Sharma <rahulsharma.cse00@gmail.com>
2024-03-04 02:19:24 +0530

    profile update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["*"],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
